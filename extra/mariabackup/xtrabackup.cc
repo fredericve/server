844916092fcdf21ae84d08c0745fb35e0df6d685
Thirunarayanan Balathandayuthapani <thiru@mariadb.com>
2025-07-07 13:14:13 +0530

    MDEV-22250 InnoDB: Failing assertion: opt_no_lock during mariabackup --backup
    backup_file_op_fail(): Ignore the FTS internal table if it is being
    created in late phase of backup. mariabackup --prepare should be
    handle intermediate table and orphaned fts internal table.

--- extra/mariabackup/xtrabackup.cc
+++ extra/mariabackup/xtrabackup.cc
@@ -113,6 +113,7 @@ Street, Fifth Floor, Boston, MA 02110-1335 USA
 #include <derror.h>
 #include <thr_timer.h>
 #include "backup_debug.h"
+#include "fts0types.h"
 
 #define MB_CORRUPTED_PAGES_FILE "innodb_corrupted_pages"
 
@@ -124,6 +125,8 @@ int sd_notifyf() { return 0; }
 
 int sys_var_init();
 
+extern const char* fts_common_tables[];
+extern const  fts_index_selector_t fts_index_selector[];
 /* === xtrabackup specific options === */
 #define DEFAULT_TARGET_DIR "./xtrabackup_backupfiles/"
 char xtrabackup_real_target_dir[FN_REFLEN] = DEFAULT_TARGET_DIR;
@@ -960,6 +963,35 @@ static void backup_file_op(uint32_t space_id, int type,
 	pthread_mutex_unlock(&backup_mutex);
 }
 
+/** Check whether the filename belongs to internal FTS table
+@param file_name  file name to be checked
+@return true if it is fts table or false otherwise */
+static bool check_if_fts_table(const char *file_name) {
+	const char *table_name_start = strrchr(file_name, '/');
+	if (table_name_start)
+		++table_name_start;
+	else
+		table_name_start = file_name;
+
+	if (!starts_with(table_name_start,"FTS_"))
+		return false;
+
+	const char *table_name_end = strrchr(table_name_start, '.');
+	if (!table_name_end)
+		table_name_end = table_name_start + strlen(table_name_start);
+	ptrdiff_t table_name_len = table_name_end - table_name_end;
+
+	for (const char **suffix = fts_common_tables; *suffix; ++suffix)
+		if (!strncmp(table_name_start, *suffix, table_name_len))
+			return true;
+	for (size_t i = 0; fts_index_selector[i].suffix; ++i)
+		if (!strncmp(table_name_start, fts_index_selector[i].suffix,
+			table_name_len))
+			return true;
+
+	return false;
+}
+
 
 /*
  This callback is called if DDL operation is detected,
@@ -982,6 +1014,20 @@ static void backup_file_op_fail(uint32_t space_id, int type,
 			space_id, int(len), name);
 		fail = !check_if_skip_table(
 				filename_to_spacename(name, len).c_str());
+		if (fail && !opt_no_lock &&
+		    check_if_fts_table(
+			filename_to_spacename(name, len).c_str())) {
+			/* Ignore the FTS internal table because InnoDB does
+			create intermediate table and their associative FTS
+			internal table when table is being rebuilt during
+			prepare phase. Also, backup_set_alter_copy_lock()
+			downgrades the MDL_BACKUP_DDL before prepare phase
+			of alter. This leads to the FTS internal table being
+			created in the late phase of backup.
+			mariabackup --prepare should be able to handle
+			this case. */
+			fail = false;
+		}
                 error= "create";
 		break;
 	case FILE_MODIFY:
